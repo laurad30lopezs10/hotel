0d79d3184b75d61f0790561c99ae47e4d8421577
Laura <laura.daniela.lopez.3010@gmail.com>
2024-04-16 23:16:33 -0500

    Se añadio termino la activity main 2 para realizar el registro de un nuevo huesped y se inicio activity main 3

--- app/src/main/java/corte2/hotel/MainActivity2.java
+++ app/src/main/java/corte2/hotel/MainActivity2.java
@@ -1,6 +1,10 @@
 package corte2.hotel;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +13,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    private EditText NombreH = (EditText) findViewById(R.id.IngresoNombreHuesped);
+    private EditText ApellidoH = (EditText) findViewById(R.id.IngresoApellidoHuesped);
+    private EditText IdentificacionH=(EditText) findViewById(R.id.IngresoNumIdentidadHuesped);
+    private EditText TelefonoH=(EditText) findViewById(R.id.IngresoTelHuesped);
+    private EditText EmailH=(EditText) findViewById(R.id.IngresoEmailHuesped);
+    private EditText UsuarioH=(EditText) findViewById(R.id.CreacionUsuarioHuesped);
+    private EditText PasswordH=(EditText) findViewById(R.id.CreacionPasswordHuesped);
+private Button GuardarH=(Button) findViewById(R.id.buttonGuardarRegistro);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +31,8 @@ public class MainActivity2 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }  public void GuardarRegistro(View v){
+        Intent linear=new Intent(this, MainActivity.class);
+        startActivity(linear);
     }
 }
